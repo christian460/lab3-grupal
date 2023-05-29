8c5d1ac71e91e08ffa964396af8ebf62fca1458a
Daniel Chura <dchuramo@unsa.edu.pe>
2023-05-29 18:33:18 -0500

    creando constante para accedera los archivos md

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -8,12 +10,10 @@ const markdownFolder = path.join(__dirname, 'markdown-files');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
+ 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-
 
 app.get('/api/files', (req, res) => {
   fs.readdir(markdownFolder, (err, files) => {
@@ -21,13 +21,29 @@ app.get('/api/files', (req, res) => {
       console.error(err);
       res.status(500).json({ error: 'Error al obtener la lista de archivos Markdown' });
     } else {
-      res.json({ files });
+      const markdownFiles = files.filter(file => path.extname(file).toLowerCase() === ".md");
+      res.json({ files : markdownFiles});
     }
   });
 });
 
 
+app.get('/leer', (req, res) => {
+  fs.readFile(path.join(__dirname, 'markdown-files/saludos.md'),'utf8',(err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al obtener el archivo Markdown' })
+      return
+    }  
+    res.json({ 
+      text: files.replace("#" ,'<h1>')
+      
+      
+      
+    })
+  })
+})
+
 app.listen(3000, () => {
   console.log('Servidor iniciado en http://localhost:3000');
 });
-
